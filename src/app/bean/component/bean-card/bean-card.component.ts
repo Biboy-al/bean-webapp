2801604244822a858eeb4f009b077d36761796dc
alvien <alvien.salvador@outlook.com>
2026-03-29 18:26:40 +1300

    1-b generate component base

---
+++ src/app/bean/component/bean-card/bean-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bean-card',
+  imports: [],
+  templateUrl: './bean-card.component.html',
+  styleUrl: './bean-card.component.css'
+})
+export class BeanCardComponent {
+
+}
---
+++ src/app/bean/component/bean-page/bean-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bean-page',
+  imports: [],
+  templateUrl: './bean-page.component.html',
+  styleUrl: './bean-page.component.css'
+})
+export class BeanPageComponent {
+
+}
